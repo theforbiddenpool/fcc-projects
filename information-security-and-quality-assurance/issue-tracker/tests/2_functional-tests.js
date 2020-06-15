43080788a3ba0125c3472ecacf9b5040bbbb78f9
theforbiddenpool <river.mundell@protonmail.com>
2020-06-15 17:01:21 +0100

    Add GET functional tests

--- information-security-and-quality-assurance/issue-tracker/tests/2_functional-tests.js
+++ information-security-and-quality-assurance/issue-tracker/tests/2_functional-tests.js
@@ -157,11 +157,51 @@ suite('Functional Tests', function() {
       });
       
       test('One filter', function(done) {
-        done()
+        chai.request(server)
+          .get('/api/issues/test')
+          .query({
+            open: true
+          })
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.isArray(res.body)
+            assert.property(res.body[0], 'issue_title');
+            assert.property(res.body[0], 'issue_text');
+            assert.property(res.body[0], 'created_on');
+            assert.property(res.body[0], 'updated_on');
+            assert.property(res.body[0], 'created_by');
+            assert.property(res.body[0], 'assigned_to');
+            assert.property(res.body[0], 'open');
+            assert.property(res.body[0], 'status_text');
+            assert.property(res.body[0], '_id');
+            assert.isTrue(res.body[0].open)
+            done()
+          })
       });
       
       test('Multiple filters (test for multiple fields you know will be in the db for a return)', function(done) {
-        done()
+        chai.request(server)
+          .get('/api/issues/test')
+          .query({
+            open: true,
+            status_text: 'In QA'
+          })
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.isArray(res.body)
+            assert.property(res.body[0], 'issue_title');
+            assert.property(res.body[0], 'issue_text');
+            assert.property(res.body[0], 'created_on');
+            assert.property(res.body[0], 'updated_on');
+            assert.property(res.body[0], 'created_by');
+            assert.property(res.body[0], 'assigned_to');
+            assert.property(res.body[0], 'open');
+            assert.property(res.body[0], 'status_text');
+            assert.property(res.body[0], '_id');
+            assert.isTrue(res.body[0].open)
+            assert.equal(res.body[0].status_text, 'In QA')
+            done()
+          })
       });
       
     });
