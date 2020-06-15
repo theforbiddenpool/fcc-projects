b78183e9c0c872e4760865b9ac8939aee85d544c
theforbiddenpool <river.mundell@protonmail.com>
2020-06-15 14:05:24 +0100

    Add helmet to prevent cross site scripting

--- information-security-and-quality-assurance/issue-tracker/server.js
+++ information-security-and-quality-assurance/issue-tracker/server.js
@@ -4,6 +4,7 @@ var express     = require('express');
 var bodyParser  = require('body-parser');
 var expect      = require('chai').expect;
 var cors        = require('cors');
+const helmet = require('helmet')
 
 var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -11,11 +12,11 @@ var runner            = require('./test-runner');
 
 var app = express();
 
+app.use(helmet.xssFilter())
+
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
-
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
