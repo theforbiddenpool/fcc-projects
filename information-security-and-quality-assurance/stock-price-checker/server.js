862951947953bdf5b99842cf4288fdb8970f9c2c
theforbiddenpool <river.mundell@protonmail.com>
2020-06-23 16:20:08 +0100

    Change var to const

--- information-security-and-quality-assurance/stock-price-checker/server.js
+++ information-security-and-quality-assurance/stock-price-checker/server.js
@@ -1,15 +1,25 @@
 'use strict';
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const expect      = require('chai').expect;
+const cors        = require('cors');
+const helmet = require('helmet')
+const cookieParser = require('cookie-parser')
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+const apiRoutes         = require('./routes/api.js');
+const fccTestingRoutes  = require('./routes/fcctesting.js');
+const runner            = require('./test-runner');
 
-var app = express();
+const app = express();
+
+app.use(helmet.contentSecurityPolicy({ directives: {
+  defaultSrc: ["'self'"],
+  scriptSrc: ["'self'"],
+  styleSrc: ["'self'"]
+}}))
+
+app.use(cookieParser())
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
@@ -46,7 +56,7 @@ app.listen(process.env.PORT || 3000, function () {
       try {
         runner.run();
       } catch(e) {
-        var error = e;
+        const error = e;
           console.log('Tests are not valid:');
           console.log(error);
       }
