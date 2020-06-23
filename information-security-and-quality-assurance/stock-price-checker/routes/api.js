fc72ac278618c3e6e1fc80857e786af78dbafbfb
theforbiddenpool <river.mundell@protonmail.com>
2020-06-23 16:22:40 +0100

    Add /api/stock-prices route

--- information-security-and-quality-assurance/stock-price-checker/routes/api.js
+++ information-security-and-quality-assurance/stock-price-checker/routes/api.js
@@ -8,16 +8,39 @@
 
 'use strict';
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
+const expect = require('chai').expect;
+const MongoClient = require('mongodb').MongoClient;
+const stockHandler = require('../controllers/stockHandler')
 
-const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+function formatData(req, res, next) {
+  if(Array.isArray(req.query.stock)) {
+    req.query.stock = req.query.stock.map(a => a.toUpperCase())
+  } else {
+    req.query.stock = req.query.stock.toUpperCase()
+    req.query.stock = [req.query.stock]
+  }
+
+  next()
+}
 
 module.exports = function (app) {
 
   app.route('/api/stock-prices')
-    .get(function (req, res){
+    .get(formatData, async function (req, res) {
+      let liked = req.cookies.liked
+
+      if(!liked) {
+        liked = []
+      }
       
+      const response = await stockHandler.getStock(req.query, liked)
+      
+      if(req.query.like) {
+        liked = Array.from(new Set([...liked, ...req.query.stock]))
+      }
+
+      res.cookie('liked', liked, { maxAge: 900000 })
+      res.json(response)
     });
     
 };
